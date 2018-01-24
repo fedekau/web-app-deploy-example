9c5d21f6e10e1d9eb19bb78223641941343992e8
fedekau <federico@wyeworks.com>
2018-01-24 08:30:14 -0300

    Generatle backend-app cookbook

---
+++ backend-app/chef/cookbooks/backend_app/README.md
@@ -0,0 +1,4 @@
+# backend_app
+
+TODO: Enter the cookbook description here.
+
---
+++ backend-app/chef/cookbooks/backend_app/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: backend_app
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
