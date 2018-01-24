2dd47470ebf1dca339849624f846a215253406c1
fedekau <federico@wyeworks.com>
2018-01-24 09:02:52 -0300

    Install git with chef

--- backend-app/chef/cookbooks/backend_app/recipes/default.rb
+++ backend-app/chef/cookbooks/backend_app/recipes/default.rb
@@ -3,6 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+apt_update 'update'
+
+apt_package 'git'
 
 git "/home/ubuntu/web-app-deploy-example" do
   repository "https://github.com/fedekau/web-app-deploy-example.git"
