032cbcc50de425642f43977e8a1b918baf701d30
fedekau <federico@wyeworks.com>
2018-01-24 08:59:27 -0300

    Clone repo with chef

--- backend-app/chef/cookbooks/backend_app/recipes/default.rb
+++ backend-app/chef/cookbooks/backend_app/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+git "/home/ubuntu/web-app-deploy-example" do
+  repository "https://github.com/fedekau/web-app-deploy-example.git"
+  revision "master"
+  action :sync
+end
