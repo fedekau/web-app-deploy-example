4e3738658781fe3fe94d83ae59e648be89ac5f90
fedekau <federico@wyeworks.com>
2018-01-24 10:53:25 -0300

    Sync repo via ssh deploy key

--- backend-app/chef/cookbooks/backend_app/recipes/default.rb
+++ backend-app/chef/cookbooks/backend_app/recipes/default.rb
@@ -8,7 +8,7 @@ apt_update 'update'
 apt_package 'git'
 
 git "/home/ubuntu/web-app-deploy-example" do
-  repository "https://github.com/fedekau/web-app-deploy-example.git"
+  repository "ext::ssh -i /home/ubuntu/.ssh/id_rsa_deploy -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no git@github.com %S /fedekau/web-app-deploy-example.git"
   revision "master"
   action :sync
 end
